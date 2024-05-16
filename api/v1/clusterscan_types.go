ba7959014ec3aa5524f959c9aa8f2f5b70bccb4a
John Wang <yuxuan.wang@mail.mcgill.ca>
2024-05-16 02:11:09 -0700

    kubebuilder init and api creation

---
+++ api/v1/clusterscan_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ClusterScanSpec defines the desired state of ClusterScan
+type ClusterScanSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of ClusterScan. Edit clusterscan_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// ClusterScanStatus defines the observed state of ClusterScan
+type ClusterScanStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ClusterScan is the Schema for the clusterscans API
+type ClusterScan struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterScanSpec   `json:"spec,omitempty"`
+	Status ClusterScanStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ClusterScanList contains a list of ClusterScan
+type ClusterScanList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterScan `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterScan{}, &ClusterScanList{})
+}
